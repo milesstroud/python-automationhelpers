4ee8daf5d36621300ed180912e0484b5f308be1d
milesstroud <40049797+milesstroud@users.noreply.github.com>
2020-12-20 15:39:43 -0500

    Simplified playlist calls, added replace option

--- Reddit Playlist Builder.py
+++ Reddit Playlist Builder.py
@@ -8,10 +8,10 @@ import spotipy.util as util
 from spotipy.oauth2 import SpotifyOAuth
 import random
 
-SPOTIPY_CLIENT_ID = ''
-secret = ''
+SPOTIPY_CLIENT_ID = '2cfb716804634481b678117914a9d6cd'
+secret = 'ecb207bc67d14a71a2841fbdef1bff6c'
 redirect_uri = 'http://localhost'
-username = ''
+username = '1211908926'
 
 scope = 'playlist-modify-public'
 token = SpotifyOAuth(scope=scope,username=username, client_id=SPOTIPY_CLIENT_ID,client_secret=secret, redirect_uri=redirect_uri)
@@ -21,9 +21,9 @@ tracks = []
 
 
 #PRAW Read Only Instance (https://praw.readthedocs.io/en/latest/getting_started/quick_start.html)
-reddit = praw.Reddit(client_id = '',
-                     client_secret = '',
-                     user_agent = '')
+reddit = praw.Reddit(client_id = 'w4dmPyZu0rVYOw',
+                     client_secret = 'mVwazkh1iKvNtMoBiH-GrAs4KmE',
+                     user_agent = 'favorite tunes by /u/snootify')
 
 #Dictionary to store all of the data retrieved
 master_dict = {"url": []}
@@ -56,12 +56,14 @@ def PCmusic():
     for post in pcposts:
         master_dict['url'].append(post.url)
 
+def convert(tracks):
+    return tuple(tracks)
+
 #Calling each function to perform their duties before creating the email
 Pop()
 HipHop()
 PsychRock()
 PCmusic()
-
 
 #Looping through either a playlist, track, or album
 for url in master_dict['url']:
@@ -82,7 +84,10 @@ for url in master_dict['url']:
 
 
 #Need first 23 characters (the length of a track id) to control for a quirk with playlist tracks
-tracks = [[track[0:22]] for track in tracks]
+# tracks = [[track[0:22]] for track in tracks]
+tracks = [track[0:22] for track in tracks]
+
+
 
 #50 Random songs to not have long albums take up space
 if len(tracks) > 50:
@@ -90,6 +95,9 @@ if len(tracks) > 50:
 else:
     tracks = tracks
 
-for track in tracks:
-    sp.playlist_add_items('{your_playlist_id}', track)
+tracks = convert(tracks)
+sp.playlist_add_items('2X7dRDEE3rFzSS7Opipohb', tracks)
+
+#To Replace Instead of Add New, Uncomment This:
+# sp.playlist_replace_items('2X7dRDEE3rFzSS7Opipohb', tracks)
 
