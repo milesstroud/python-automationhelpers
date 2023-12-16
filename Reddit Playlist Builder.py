3ae09803d1aec7338da2cd2d98e2441ca9771d7e
milesstroud <40049797+milesstroud@users.noreply.github.com>
2023-12-16 18:37:47 -0500

    Clean up - organize into one main function

--- Reddit Playlist Builder.py
+++ Reddit Playlist Builder.py
@@ -38,76 +38,46 @@ song_titles = []
 
 #Function to remove URL Hostile string components
 def urlify(s):
-
     # Remove all non-word characters (everything except numbers and letters)
     s = re.sub(r"[^\w\s]", '', s)
-
     # Replace all runs of whitespace with a single dash
     s = re.sub(r"\s+", '+', s)
-
     return s
-
-
-#Defining functions to build the master dictionary using data from each subreddit
-def Pop():
-    popreddit = reddit.subreddit('Popheads') #Defining subreddit of choice (first, popheads)
-    popemergencies = popreddit.top(limit =35, time_filter = 'day') #Retrieving the top 5 posts within the last 24hours
-    for post in popemergencies:
-        master_dict['url'].append(post.url)
-        print(master_dict)
-
-#Repeat for Subs of Choice!
-def HipHop():
-    hiphopheads = reddit.subreddit('hiphopheads')
-    HHH = hiphopheads.top(limit=30, time_filter = 'day')
-    for post in HHH:
-        master_dict['url'].append(post.url)
-        print(master_dict)
-
-
-def PsychRock():
-    psychrock = reddit.subreddit('psychedelicrock')
-    psychposts = psychrock.top(limit=10, time_filter = 'day')
-    for post in psychposts:
-        master_dict['url'].append(post.url)
-        print(master_dict)
-
-def PCmusic():
-    pcmusic = reddit.subreddit('pcmusic')
-    pcposts = pcmusic.top(limit=15, time_filter = 'day')
-    for post in pcposts:
-        master_dict['url'].append(post.url)
-        print(master_dict)
-
-
-def indieheads():
-    indieheads = reddit.subreddit('indieheads')
-    indietunes = indieheads.top(limit=15, time_filter = 'day')
-    for post in indietunes:
-        master_dict['url'].append(post.url)
-        print(master_dict)
 
 def convert(tracks):
     return tuple(tracks)
 
-#Calling each function to perform their duties before creating the email
-Pop()
-HipHop()
-PsychRock()
-PCmusic()
-indieheads()
+subreddits = {'Popheads' : 35,
+              'hiphopheads' : 35,
+              'psychedelicrock' : 15,
+              'pcmusic' : 10,
+              'indieheads' : 20}
+
+#Defining functions to build the master dictionary using data from each subreddit
+for key in subreddits:
+    subreddit = reddit.subreddit(key)
+    jams = subreddit.top(limit=subreddits[key], time_filter = 'day')
+    for tune in jams:
+        master_dict['url'].append(tune.url)
+
 
 #Looping through either a playlist, track, or album
 for url in master_dict['url']:
     if url.startswith('https://open.spotify.com/playlist/'):
         playlist_id = url[34:]
         print(playlist_id)
-        for item in sp.playlist(playlist_id)['tracks']['items']:
-            tracks.append(item['track']['id'])
+        try:
+            for item in sp.playlist(playlist_id)['tracks']['items']:
+                tracks.append(item['track']['id'])
+        except:
+            pass
     elif url.startswith('https://open.spotify.com/track/'):
         track_id = url[31:]
         print(track_id)
-        tracks.append(track_id)
+        try:
+            tracks.append(track_id)
+        except:
+            pass
     elif url.startswith('https://open.spotify.com/album/'):
         album_id = url[31:]
         print(album_id)
@@ -120,7 +90,11 @@ for url in master_dict['url']:
         video_id = url[32:43]
         response = requests.get('https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=' + video_id + '&key=' + API_KEY)
         response = json.loads(response.content)
-        song_titles.append(response['items'][0]['snippet']['title'])
+        try:
+            song_titles.append(response['items'][0]['snippet']['title'])
+        except Exception as e:
+            print(e)
+            pass
     elif url.startswith('https://youtu.be/'):
         video_id = url[17:28]
         response = requests.get('https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=' + video_id + '&key=' + API_KEY)
